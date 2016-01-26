3cc29965934a1aea814b751152965d6ad7852807
ben.upsilon <ben.upsilon@gmail.com>
2016-01-26 22:39:29 +0800

    继续挖坑呗.

---
+++ o/src/main/java/ben/upsilon/o/ORM.java
@@ -0,0 +1,18 @@
+package ben.upsilon.o;
+
+/**
+ * Created by ben on 1/12/16.
+ */
+public final class ORM {
+
+
+    private void createDataBase(){
+
+    }
+
+
+    public static void main(String[] args){
+            System.out.println("omg");
+    }
+
+}
