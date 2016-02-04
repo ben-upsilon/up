ac79384aefc39ca29e3873b7f1efe85d725a65be
ben.upsilon <ben.upsilon@gmail.com>
2016-02-04 21:32:18 +0800

    清理代码.

--- app/src/main/java/ben/upsilon/up/net/CellularNetworks.java
+++ app/src/main/java/ben/upsilon/up/net/CellularNetworks.java
@@ -1,6 +1,7 @@
 package ben.upsilon.up.net;
 
 /**
+ * 移动网络
  * Created by ben on 1/8/16.
  */
 public class CellularNetworks {
--- app/src/main/java/ben/upsilon/up/net/Wifi.java
+++ app/src/main/java/ben/upsilon/up/net/Wifi.java
@@ -1,6 +1,7 @@
 package ben.upsilon.up.net;
 
 /**
+ * wifi网络
  * Created by ben on 1/8/16.
  */
 public class Wifi {
