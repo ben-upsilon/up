e1956f992b44de2bc9c7bfaf7800c909282f62d1
ben.upsilon <ben.upsilon@qq.com>
2020-04-01 11:23:18 +0800

    fix build error1

--- syncup/src/main/java/ben/upsilon/syncup/FilesActivity.kt
+++ syncup/src/main/java/ben/upsilon/syncup/FilesActivity.kt
@@ -4,7 +4,4 @@ import android.app.Activity
 import android.os.Bundle
 
 class FilesActivity : Activity() {
-    override fun onCreate(savedInstanceState: Bundle) {
-        super.onCreate(savedInstanceState)
-    }
 }
